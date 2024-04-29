a3b44f0e35d78acebabd061b8438b9ac4f4a5a78
J1342 <alex.osipov.oa@gmail.com>
2024-04-29 18:02:40 +0300

    Parsing tasks were created.

---
+++ parser/tasks.py
@@ -0,0 +1,57 @@
+import requests
+from user_agent import generate_user_agent
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from requests.exceptions import HTTPError, RequestException
+from celery import Celery
+import celery
+
+
+app = Celery('tasks', broker='pyamqp://guest@localhost//')
+
+
+class ParseHtml(celery.Task):
+
+    name = 'parse-html'
+
+    def run(self, url):
+        """Get html page and run xml parsing"""
+        headers = {
+            'User-Agent': generate_user_agent()
+        }
+        try:
+            response = requests.get(url, headers=headers)
+            if response.status_code != 200:
+                raise HTTPError()
+            soup = BeautifulSoup(response.text, 'html.parser')
+            atags = soup.select('.w-space-nowrap a[target]')
+            links = [urljoin('https://zakupki.gov.ru',
+                             tag.get('href').replace('view.html', 'viewXml.html'))
+                     for tag in atags]
+        except (RequestException, Exception) as exc:
+            raise self.retry(exc=exc)
+        for link in links:
+            parse_xml.delay(link)
+
+
+class ParseXml(celery.Task):
+
+    name = 'parse-xml'
+
+    def run(self, url):
+        """Get xml page then parse and print result"""
+        headers = {
+            'User-Agent': generate_user_agent()
+        }
+        try:
+            response = requests.get(url, headers=headers)
+            if response.status_code != 200:
+                raise HTTPError()
+            soup = BeautifulSoup(response.text, 'xml')
+        except (RequestException, Exception) as exc:
+            raise self.retry(exc=exc)
+        print('{}: {}'.format(url, soup.publishDTInEIS.string))
+
+
+parse_html = app.register_task(ParseHtml())
+parse_xml = app.register_task(ParseXml())
